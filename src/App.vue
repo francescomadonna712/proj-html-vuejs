8c8846b58e8a78b6060c8dd4d704fdd80156a996
francesco madonna <fra.mad.93@hotmail.it>
2024-06-06 11:38:56 +0200

    progetto unito e finito (quasi)

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,41 @@
 <script>
-import AppHeader from "./components/AppHeader.vue";
-import AppFooter from "./components/AppFooter.vue";
-import AppCardHeader from "./components/AppCardHeader.vue";
-
-
+import store from './data/store.js';
+import AppMain from './components/AppMain.vue';
+import AppCarousel from './components/AppCarousel.vue';
+import AppCarouselMini from './components/AppCarouselMini.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+import Player from './components/Player.vue';
 
 export default {
   components: {
+    AppMain,
+    AppCarousel,
+    AppCarouselMini,
     AppHeader,
     AppFooter,
-    AppCardHeader,
-
-
-
-
-
+    Player,
   },
-
   data() {
     return {
-
-    }
+      store,
+    };
   },
-}
+  methods: {
+    getImage(nomefile) {
+      return new URL(`./assets/${nomefile}`, import.meta.url);
+    },
+  },
+};
 </script>
 
 <template>
   <AppHeader />
-
+  <AppCarousel />
+  <AppMain />
+  <AppCarouselMini />
+  <Player />
   <AppFooter />
-
-
-
-
-
-
 </template>
 
 <style scoped></style>
--- src/data/store.js
+++ src/data/store.js
@@ -95,34 +95,36 @@ let store = reactive({
   },
   // ---------------> sezione dedicata ad header e footer di Francesco <--------------
   navLinks: [
-    { href: "#", icon: "fas fa-home", text: "HOME", active: true },
-    { href: "#", icon: "fas fa-user", text: "ABOUT US", active: false }
+    { href: '#', icon: 'fas fa-home', text: 'HOME', active: true },
+    { href: '#', icon: 'fas fa-user', text: 'ABOUT US', active: false },
   ],
   dropdowns: [
     {
-      text: "LIFESTYLE",
-      icon: "fa-solid fa-suitcase",
-
+      text: 'LIFESTYLE',
+      icon: 'fa-solid fa-suitcase',
     },
     {
-      text: "STORIES",
-      icon: "fa-solid fa-book-open-reader",
-
+      text: 'STORIES',
+      icon: 'fa-solid fa-book-open-reader',
     },
     {
-      text: "PAGES",
-      icon: "fa-solid fa-book",
+      text: 'PAGES',
+      icon: 'fa-solid fa-book',
       items: [
-        { href: "#", icon: "fas fa-search", text: "SEARCH RESULTS" },
-        { href: "#", icon: "fa-solid fa-box-archive", text: "CATEGORY ARCHIVE" },
-        { href: "#", icon: "fas fa-user", text: "AUTHOR ARCHIVE" },
-        { href: "#", icon: "fa-solid fa-calendar-days", text: "DATE ARCHIVE" },
-        { href: "#", icon: "fa-solid fa-bug", text: "ERROR 404" }
-      ]
-    }
+        { href: '#', icon: 'fas fa-search', text: 'SEARCH RESULTS' },
+        {
+          href: '#',
+          icon: 'fa-solid fa-box-archive',
+          text: 'CATEGORY ARCHIVE',
+        },
+        { href: '#', icon: 'fas fa-user', text: 'AUTHOR ARCHIVE' },
+        { href: '#', icon: 'fa-solid fa-calendar-days', text: 'DATE ARCHIVE' },
+        { href: '#', icon: 'fa-solid fa-bug', text: 'ERROR 404' },
+      ],
+    },
   ],
   bottomLinks: [
-    { href: "#", icon: "fa-solid fa-envelope", text: "CONTACT US" }
+    { href: '#', icon: 'fa-solid fa-envelope', text: 'CONTACT US' },
   ],
 
   header: {
@@ -149,25 +151,25 @@ let store = reactive({
   },
   footerData: [
     {
-      title: "GET STARTED",
-      links: ["Resources", "Tutorials", "Examples", "Docs"]
+      title: 'GET STARTED',
+      links: ['Resources', 'Tutorials', 'Examples', 'Docs'],
     },
     {
-      title: "ABOUT",
-      links: ["Stories", "Community", "Blog", "Brand Assets"]
+      title: 'ABOUT',
+      links: ['Stories', 'Community', 'Blog', 'Brand Assets'],
     },
     {
-      title: "FEATURES",
-      links: ["Overview", "Design", "Code", "Collaborate"]
+      title: 'FEATURES',
+      links: ['Overview', 'Design', 'Code', 'Collaborate'],
     },
     {
-      title: "QUICK LINKS",
-      links: ["Stories", "Community", "Blog", "Brand Assets"]
+      title: 'QUICK LINKS',
+      links: ['Stories', 'Community', 'Blog', 'Brand Assets'],
     },
     {
-      title: "SUBSCRIBE TO NEWSLETTER",
-      links: []
-    }
+      title: 'SUBSCRIBE TO NEWSLETTER',
+      links: [],
+    },
   ],
   // ---------------> sezione dedicata ai caroselli di Arnaldo <--------------
   carousel: {
@@ -175,12 +177,97 @@ let store = reactive({
   },
   // ---------------> sezione dedicata al main di Giacomo <--------------
   main: {
-    // da riempire all'occorrenza
+    mainPost: '',
+    temporaryPost: [
+      {
+        title: 'Live Ideas You Might Be Anime',
+        date: 'December 26, 2022',
+        badge: ['Culture', 'Stories'],
+        img: 'stories.webp',
+      },
+      {
+        title: 'Reasons To Visit France',
+        date: 'December 26, 2022',
+        badge: ['Lifestyle', 'Travel'],
+        img: 'visit.webp',
+      },
+      {
+        title: 'Traveling Alone Is Awesome',
+        date: 'December 26, 2022',
+        badge: ['Stories', 'Travel'],
+        img: 'travel.webp',
+      },
+      {
+        title: 'The Best Success Stories',
+        date: 'December 26, 2022',
+        badge: ['Culture', 'Stories'],
+        img: 'success.webp',
+      },
+      {
+        title: 'Places For A Road Trip',
+        date: 'December 26, 2022',
+        badge: ['Lifestyle', 'Stories', 'Travel'],
+        img: 'landscape.webp',
+      },
+      {
+        title: 'Music The Love Of My Life',
+        date: 'December 26, 2022',
+        badge: ['Culture', 'Lifestyle'],
+        img: 'music.webp',
+      },
+      {
+        title: 'Fashion Trend Now A Days',
+        date: 'December 26, 2022',
+        badge: ['Fashion', 'Lifestyle'],
+        img: 'fashion.webp',
+      },
+    ],
   },
   // ---------------> sezione dedicata ai video trailer di Samian <--------------
-  trailers: {
-    // da riempire all'occorrenza
-  },
+  trailers: [
+    {
+      id: 1,
+      title: 'Eternal Cultivation Anime',
+      embedUrl: 'https://www.youtube.com/embed/9rYkZlt2hHE',
+      videoId: '9rYkZlt2hHE',
+    },
+    {
+      id: 2,
+      title: "Sun's Out, Guns Out",
+      embedUrl: 'https://www.youtube.com/embed/zlwQERpksnw',
+      videoId: 'zlwQERpksnw',
+    },
+    {
+      id: 3,
+      title: 'Top 10 Vinland Saga Moments',
+      embedUrl: 'https://www.youtube.com/embed/itQnUDMMhaU',
+      videoId: 'itQnUDMMhaU',
+    },
+    {
+      id: 4,
+      title: 'Everytime Sukuna Appears',
+      embedUrl: 'https://www.youtube.com/embed/IrU2dklT11s',
+      videoId: 'IrU2dklT11s',
+    },
+    {
+      id: 5,
+      title: 'Wizard King (Julius) vs Licht (Patri) Full Fight',
+      embedUrl: 'https://www.youtube.com/embed/H5qupR6YQpQ',
+      videoId: 'H5qupR6YQpQ',
+    },
+    {
+      id: 6,
+      title: 'Vinland Saga | SHATTERED',
+      embedUrl: 'https://www.youtube.com/embed/ICt1jYfucec',
+      videoId: 'ICt1jYfucec',
+    },
+    {
+      id: 7,
+      title: 'Konan Arranges',
+      embedUrl: 'https://www.youtube.com/embed/CkC5CtabT1Y',
+      videoId: 'CkC5CtabT1Y',
+    },
+  ],
 });
 
 export default store;
